facabbeade0bb06f08519dc8d07b77727cbf153f
Ignacio Manríquez <i.manriquez01@ufromail.cl>
2024-05-03 16:50:38 -0400

    Commit v0.2
    Updates

--- especializacion-vue/src/App.vue
+++ especializacion-vue/src/App.vue
@@ -1,26 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--  -->
+  <ButtomCounter></ButtomCounter>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ButtonCounter from './components/ButtonCounter.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ButtonCounter
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
